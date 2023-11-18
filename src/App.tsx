cefdb7ddc537112514a83a7077865b9098f96320
Ewan Bauld <ewan.bauld@gmail.com>
2023-11-18 22:22:55 +0000

    Comment out StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Directory from './components/directory/directory';
+import { root } from './data/files';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Bright Tree</h1>
+        <Directory root={root}/>
       </header>
     </div>
   );
--- src/components/directory/directory.tsx
+++ src/components/directory/directory.tsx
@@ -13,9 +13,20 @@ export type IFiles = {
 
   const Directory: React.FC<IDirectoryProps> = ({ root }) => {
 
+    const renderNode = (node: IFiles, index: number) => (
+        <div key={index} className="element">
+          {node.type === 'folder' ? (
+            <strong>{node.name}</strong>
+          ) : (
+            <span>{node.name}</span>
+          )}
+          {node.files && node.files.map(renderNode)}
+        </div>
+      );
+
     return (
         <div>
-            {root.files?.map((file, i) => <div key={i}>{file.name}</div>)}
+            {root.files?.map(renderNode)}
         </div>
     )
   };
