e59528b6889a0ac69d62074eba48ba0497db6be0
Ewan Bauld <ewan.bauld@gmail.com>
2023-11-26 15:35:55 +0000

    Spilt out Filter component and add test

---
+++ src/components/Filter/Filter.test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import Filter from "./Filter";
+
+describe("Filter", () => {
+  it("renders Filter component correctly", () => {
+    const filter = "test filter";
+    const handleFilterChangeMock = jest.fn();
+
+    render(
+      <Filter filter={filter} handleFilterChange={handleFilterChangeMock} />
+    );
+
+    expect(screen.getByPlaceholderText("Filter directory...")).toBeInTheDocument();
+    expect(screen.getByTestId("filterInput")).toHaveValue(filter);
+  });
+
+  it("triggers handleFilterChange when input value changes", () => {
+    const filter = "test filter";
+    const handleFilterChangeMock = jest.fn();
+
+    render(
+      <Filter filter={filter} handleFilterChange={handleFilterChangeMock} />
+    );
+
+    fireEvent.change(screen.getByTestId("filterInput"), { target: { value: "new filter" } });
+
+    expect(handleFilterChangeMock).toHaveBeenCalled();
+  });
+});
---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface IFilterProps {
+    filter: string;
+    handleFilterChange: (filter: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Filter: React.FC<IFilterProps> = ({ filter, handleFilterChange }) => {
+    return (
+        <div className="filter-container">
+        <input
+          type="text"
+          placeholder="Filter directory..."
+          value={filter}
+          onChange={handleFilterChange}
+          data-testid={'filterInput'}
+          className="filter"
+        />
+      </div>
+    )
+};
+export default Filter;
